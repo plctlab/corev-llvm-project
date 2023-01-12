0f7e50e6011a12c004241b16767ed7114c2bea11
Qihan Cai <qcai8733@uni.sydney.edu>
2023-01-12 09:42:54 +0800

    [corev]fix hwlp instructions
    Updated hwlp instructions for CV32E40P from version 1 to version 2 according to the documentation.

---
+++ llvm/test/MC/RISCV/corev/hwlp/end-invalid.s
@@ -0,0 +1,17 @@
+# RUN: not llvm-mc -triple=riscv32 --mattr=+xcorevhwlp %s 2>&1 \
+# RUN:        | FileCheck %s --check-prefixes=CHECK-ERROR
+
+cv.end 2, 0
+# CHECK-ERROR: immediate must be an integer in the range [0, 1]
+
+cv.end -1, 0
+# CHECK-ERROR: immediate must be an integer in the range [0, 1]
+
+cv.end 1, 10
+# CHECK-ERROR: invalid operand for instruction
+
+cv.end 1
+# CHECK-ERROR: too few operands for instruction
+
+cv.end 1, s2, 0
+# CHECK-ERROR: invalid operand for instruction
---
+++ llvm/test/MC/RISCV/corev/hwlp/start-invalid.s
@@ -0,0 +1,17 @@
+# RUN: not llvm-mc -triple=riscv32 --mattr=+xcorevhwlp %s 2>&1 \
+# RUN:        | FileCheck %s --check-prefixes=CHECK-ERROR
+
+cv.start 2, 0
+# CHECK-ERROR: immediate must be an integer in the range [0, 1]
+
+cv.start -1, 0
+# CHECK-ERROR: immediate must be an integer in the range [0, 1]
+
+cv.start 1, 10
+# CHECK-ERROR: invalid operand for instruction
+
+cv.start 1
+# CHECK-ERROR: too few operands for instruction
+
+cv.start 1, s2, 0
+# CHECK-ERROR: invalid operand for instruction
